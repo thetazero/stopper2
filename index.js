f1d0dc773b32e088ecfaf45282e2504663d06a13
Aleksei Seletskiy <lesha.seletskiy@gmail.com>
2023-12-06 21:39:41 -0500

    base stopper impl

--- index.js
+++ index.js
@@ -1,30 +1,74 @@
+const timeElem = document.getElementById('time');
+
 let active = false;
 let start_time = Date.now();
 let max_ellapsed_time = 5000; // ms
 let ellapsed_time = 0;
+let game_state = 'inactive';
 
 function main_click() {
-    if (active) {
-        ellapsed_time += Date.now() - start_time;
+    if (game_state === 'game_over') {
+        game_state = 'inactive';
+        set_accent(accent.inactive);
+    } else if (game_state === 'inactive') {
+        game_state = 'active';
+        set_accent(accent.active);
+        start_time = Date.now();
+        active = true;
     } else {
-        start_time = Date.now()
-    }
+        if (active) {
+            ellapsed_time += Date.now() - start_time;
+        } else {
+            start_time = Date.now()
+        }
 
-    active = !active;
+        active = !active;
+    }
 }
 
-const timeElem = document.getElementById('time');
+function set_display_time(time) {
+    timeElem.innerText = (time / 1000).toFixed(3);
+}
+
+function set_accent(color) {
+    document.documentElement.style.setProperty('--accent', color);
+}
+
+const accent = {
+    game_over: 'hsl(0, 50%, 50%)',
+    active: 'hsl(451, 50%, 50%)',
+    inactive: 'hsl(451, 20%, 50%)',
+}
+
 setInterval(() => {
     if (!active) return;
     let cur_ellapsed_time = Date.now() - start_time;
 
     let total_time = cur_ellapsed_time + ellapsed_time;
 
-    timeElem.innerText = (total_time/1000).toFixed(2);
+    set_display_time(total_time);
 
     if (total_time > max_ellapsed_time) {
         active = false;
-        timeElem.innerText = 'gg';
+        set_display_time(0);
         ellapsed_time = 0;
+        game_state = 'game_over';
+        set_accent(accent.game_over);
     }
-}, 10)
+}, 1)
+
+window.addEventListener('resize', () => {
+    fix_window_sizing()
+});
+
+function fix_window_sizing() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+fix_window_sizing()
+
+document.querySelectorAll('.main_click').forEach(elem => {
+    elem.addEventListener('touchstart', main_click)
+})
+
+set_accent(accent.inactive);
